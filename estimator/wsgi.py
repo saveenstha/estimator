8002c81b96f0edd379d7f230acc75be426bc3f47
saveenstha <biz.saveen@gmail.com>
2025-08-07 19:24:02 +0545

    added DOCKERFILE for deployment, updated requirements.txt

--- estimator/wsgi.py
+++ estimator/wsgi.py
@@ -1,7 +1,11 @@
 import os
-
+import environ
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'estimator.settings')
+env = environ.Env()
+environ.Env.read_env()
+environment = env('ENVIRONMENT', default='prod')
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', f'estimator.settings.{environment}')
 
 application = get_wsgi_application()
